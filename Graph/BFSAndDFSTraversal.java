80bef472de7965b8807203151137e4a29f338fb5
arunbhati1994 <arunbhati65@gmail.com>
2020-05-21 15:47:32 +0530

    Graph BFS and DFS traversal

--- Graph/BFSAndDFSTraversal.java
+++ Graph/BFSAndDFSTraversal.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 //1       level 4
 //2 1     level 5
 //BFS print level by level
-public class BFSTraversal{
+public class BFSAndDFSTraversal{
 	
 	static void doBFSTraverse(ArrayList<ArrayList<Integer>> matrix,int v,int source,Boolean [] visited){
 		System.out.print("BFS traverse");
@@ -28,6 +28,7 @@ public class BFSTraversal{
 		}
 	}
 
+	
 	static void BFSHandleDisconnectedCases(ArrayList<ArrayList<Integer>> matrix,int v){
 		Boolean [] visited=new Boolean[v];
 		for(int source=0;source<v;++source){
@@ -37,19 +38,48 @@ public class BFSTraversal{
 		}
 	}
 
+	static void doDFSTraverse(ArrayList<ArrayList<Integer>> matrix,int v,int source,Boolean [] visited){
+		System.out.print(source+"-");
+		visited[source]=true;
+			for(int i=0;i<matrix.get(source).size();++i){
+				if(matrix.get(source).size()!=0 &&
+				  (visited[matrix.get(source).get(i)]==null || !visited[matrix.get(source).get(i)])){
+				doDFSTraverse(matrix,v,matrix.get(source).get(i),visited);
+				
+			}
+		}
+	}
+
+	static void DFSHandleDisconnectedCases(ArrayList<ArrayList<Integer>> matrix,int v){
+		System.out.print("\nDFS traverse");
+		Boolean [] visited=new Boolean[v];
+		for(int source=0;source<v;++source){
+			if(visited[source]==null || visited[source]==false){
+			doDFSTraverse(matrix,v,source,visited);
+			}
+		}
+	}
+
 	public static void main(String args[]){
-		int v=5;
+		int v=4;
 		GraphUsingList graphUsingList=new GraphUsingList(v);
 		graphUsingList.addEdge(0,1);
-		graphUsingList.addEdge(1,3);
-		graphUsingList.addEdge(2,4);
-		graphUsingList.addEdge(3,1);
-		graphUsingList.addEdge(4,1);
+		graphUsingList.addEdge(1,2);
+		graphUsingList.addEdge(2,0);
+		graphUsingList.addEdge(2,3);
+		graphUsingList.addEdge(3,3);
 
 		graphUsingList.printMatrix();
 		
 		ArrayList<ArrayList<Integer>> matrix=graphUsingList.matrix;
+
 		BFSHandleDisconnectedCases(matrix,v);
+
+		DFSHandleDisconnectedCases(matrix,v);
+
+		System.out.println("\nDFS from source 2");
+		Boolean [] visited=new Boolean[v];
+		doDFSTraverse(matrix,v,2,visited);
 		
 	}
 
