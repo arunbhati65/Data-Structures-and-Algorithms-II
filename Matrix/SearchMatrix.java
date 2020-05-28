1a06dee08bbeae27c8bd104620ad62085cd7727c
arunbhati1994 <arunbhati65@gmail.com>
2020-05-28 22:32:22 +0530

    Matrix Search

---
+++ Matrix/SearchMatrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0) return false;
+        int i=0;
+        int j=matrix[0].length-1;
+        while(i<=matrix.length-1 && j>=0){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else if(matrix[i][j]<target) i++;
+        }
+        return false;
+    }
+}
