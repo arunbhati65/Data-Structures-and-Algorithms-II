874592ec2811c917b2ef56dd21b2780009508213
arunbhati1994 <arunbhati65@gmail.com>
2020-05-31 12:51:57 +0530

    Range Sum of BST

---
+++ Tree3/RangeSumBST.java
@@ -0,0 +1,11 @@
+
+class Solution {
+    int sum=0;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root==null) return 0;
+        if(root.left!=null && root.val>=L)rangeSumBST(root.left,L,R);
+        if(root.val>=L && root.val<=R) sum=sum+root.val;
+        if(root.right!=null && root.val<=R)rangeSumBST(root.right,L,R);
+        return sum;
+    }
+}
