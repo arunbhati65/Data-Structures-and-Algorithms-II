dd9cb68153adb520a26c88dbf6c3722024d0c727
arunbhati1994 <arunbhati65@gmail.com>
2020-06-11 17:38:32 +0530

    Reverse an Integer

--- Integer1/ReverseNumber.java
+++ Integer1/ReverseNumber.java
@@ -1,3 +1,26 @@
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+*/
+class Solution {//improved
+    public int reverse(int x) {
+        boolean negative=x<0?true:false;
+        x=Math.abs(x);
+        int ans=0;
+        while(x!=0){ //123
+            int pop=x%10;
+/*
+However, this approach is dangerous, because the pop can cause overflow.
+*/
+            if (ans > Integer.MAX_VALUE/10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (ans < Integer.MIN_VALUE/10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            ans=ans*10+pop; //3
+            x=x/10;//12
+        }
+        ans=negative?(ans*-1):ans;
+        return ans;
+    }
+}
+
 class ReverseNumber {
     public int reverse(int x) {
         boolean negative=x<0?true:false;
