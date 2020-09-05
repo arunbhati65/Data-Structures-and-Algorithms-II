e7718c4effce234ccdc326bfe3c5a5a0ff726f20
arunbhati1994 <arunbhati65@gmail.com>
2020-09-06 01:51:59 +0530

    RS

--- 3_DynamicProgramming & Recursiion/LongestCommonSubsequence.java
+++ 3_DynamicProgramming & Recursiion/LongestCommonSubsequence.java
@@ -1,6 +1,23 @@
 class Solution {
     
     public int longestCommonSubsequence(String text1, String text2) {
+        int result[][]=new int[text1.length()+1][text2.length()+1];
+        for(int i=text1.length()-1;i>=0;--i){
+            for(int j=text2.length()-1;j>=0;--j){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    result[i][j]= 1+result[i+1][j+1];
+                }else{
+                    result[i][j]= Math.max(result[i+1][j],result[i][j+1]);
+                }
+            }
+        }
+        return result[0][0];
+    }
+}
+
+class Solution {
+    
+    public int longestCommonSubsequence(String text1, String text2) {
         Integer result[][]=new Integer[text1.length()+1][text2.length()+1];
        return helper(text1.toCharArray(),text2.toCharArray(),text1.length()-1,text2.length()-1,result) ;
     }
