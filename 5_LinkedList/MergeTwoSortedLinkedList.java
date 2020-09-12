9a9f723907bde265f8f5f408aaf9a5771a71ca6c
arunbhati1994 <arunbhati65@gmail.com>
2020-09-12 22:56:33 +0530

    merge 2 Sorted List

--- 5_LinkedList/MergeTwoSortedLinkedList.java
+++ 5_LinkedList/MergeTwoSortedLinkedList.java
@@ -2,6 +2,40 @@
 Merge two sorted linked lists and return it as a new list. 
 The new list should be made by splicing together the nodes of the first two lists.
  */
+class Solution{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode[] lists={l1,l2};
+        return mergeKLists(lists);
+        
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy=new ListNode();
+        if(lists.length==0) return null;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>(new Comparator<ListNode>(){
+            public int compare(ListNode n1,ListNode n2){
+                return n1.val-n2.val;
+            }            
+        });
+        for(ListNode node:lists){
+            if(node!=null){
+                pq.add(node);    
+            }
+        }
+        
+        ListNode curr=dummy;
+        while(!pq.isEmpty()){
+            ListNode node=pq.poll();
+            curr.next=node;
+            curr=curr.next;
+            if(node.next!=null){
+                pq.add(node.next);
+            }
+        }
+        return dummy.next;
+      
+    }
+}
+
 class MergeTwoSortedLinkedList{
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result=new ListNode();
