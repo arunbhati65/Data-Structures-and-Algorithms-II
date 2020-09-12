96843dd4f5ff4b5acb5be4299c0d74a985108781
arunbhati1994 <arunbhati65@gmail.com>
2020-09-12 18:48:43 +0530

    Word Search 2

--- 4_Matrix/WordSearch2.java
+++ 4_Matrix/WordSearch2.java
@@ -4,6 +4,56 @@ Each word must be constructed from letters of sequentially adjacent cell, where 
 or vertically neighboring. The same letter cell may not be used more than once in a word.
 */
 class Solution {
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> ans=new ArrayList<>();
+        if(words.length==0) return ans;
+        for(String word:words){
+            if(exist(board,word)){
+                ans.add(word);
+            }
+        }
+        return ans;
+    }
+    Boolean visited[][];
+    private boolean exist(char[][] board, String word) {
+        boolean visited[][] =new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;++i){
+            for(int j=0;j<board[0].length;++j){
+                if(board[i][j]==word.charAt(0)){
+                    if(dfs(board,visited,i,j,word,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    boolean dfs(char[][] board,boolean visited[][],int x,int y,String word,int index){
+        if(index==word.length()-1) return true;
+        visited[x][y]=true;
+        int[] xx={0,0,1,-1};
+        int[] yy={1,-1,0,0};
+        for(int i=0;i<xx.length;++i){
+            if(!isSafe(board,visited,x+xx[i],y+yy[i],word,index+1)) continue;
+            if(dfs(board,visited,x+xx[i],y+yy[i],word,index+1)){
+                return true;
+            }
+        }
+        visited[x][y]=false;
+        return false;
+    }
+    
+    boolean isSafe(char[][] board,boolean visited[][], int x, int y,String word,int index){
+        return x>=0 && x<board.length && y>=0 && y<board[0].length && !visited[x][y] && 
+            board[x][y]==word.charAt(index);
+    }
+    
+}
+
+
+class Solution {
     Boolean[][] visited;
     public List<String> findWords(char[][] board, String[] words) {
         List<String> list=new LinkedList<>();
