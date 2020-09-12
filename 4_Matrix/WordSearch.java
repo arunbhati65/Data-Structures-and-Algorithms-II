f3c0cf4e41ba328640c47485c9bb22f58cf81e96
arunbhati1994 <arunbhati65@gmail.com>
2020-09-12 18:44:16 +0530

    Word Search

--- 4_Matrix/WordSearch.java
+++ 4_Matrix/WordSearch.java
@@ -8,6 +8,43 @@ vertically neighboring. The same letter cell may not be used more than once.
 class Solution {
     Boolean visited[][];
     public boolean exist(char[][] board, String word) {
+        boolean visited[][] =new boolean[board.length][board[0].length];
+        for(int i=0;i<board.length;++i){
+            for(int j=0;j<board[0].length;++j){
+                if(board[i][j]==word.charAt(0)){
+                    if(dfs(board,visited,i,j,word,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    boolean dfs(char[][] board,boolean visited[][],int x,int y,String word,int index){
+        if(index==word.length()-1) return true;
+        visited[x][y]=true;
+        int[] xx={0,0,1,-1};
+        int[] yy={1,-1,0,0};
+        for(int i=0;i<xx.length;++i){
+            if(!isSafe(board,visited,x+xx[i],y+yy[i],word,index+1)) continue;
+            if(dfs(board,visited,x+xx[i],y+yy[i],word,index+1)){
+                return true;
+            }
+        }
+        visited[x][y]=false;
+        return false;
+    }
+    
+    boolean isSafe(char[][] board,boolean visited[][], int x, int y,String word,int index){
+        return x>=0 && x<board.length && y>=0 && y<board[0].length && !visited[x][y] && 
+            board[x][y]==word.charAt(index);
+    }
+}
+
+class Solution {
+    Boolean visited[][];
+    public boolean exist(char[][] board, String word) {
        visited=new Boolean[board.length][board[0].length]; 
        for(int i=0;i<board.length;++i){
            for(int j=0;j<board[0].length;++j){
