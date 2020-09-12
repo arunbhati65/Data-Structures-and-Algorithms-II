c0905c64d0f39e2dd62281705429aae5cc96534f
arunbhati1994 <arunbhati65@gmail.com>
2020-09-12 16:17:23 +0530

    Ziz Zag Tree Traversal optimized Code

--- 6_Tree/BinaryTree1/ZigZagLevel.java
+++ 6_Tree/BinaryTree1/ZigZagLevel.java
@@ -1,3 +1,35 @@
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null) return ans;
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        s1.push(root);
+        while(!s1.isEmpty() || !s2.isEmpty()){
+            Stack<TreeNode> curr=!s1.isEmpty()?s1:s2;
+            boolean leftToRight=!s1.isEmpty();
+            Stack<TreeNode> currEmpty=s1.isEmpty()?s1:s2;
+            int size=curr.size();
+            List<Integer> list=new ArrayList<>();
+            for(int i=0;i<size;++i){
+                TreeNode currNode=curr.pop();
+                list.add(currNode.val);
+                if(leftToRight){
+                    if(currNode.left!=null) currEmpty.push(currNode.left);
+                    if(currNode.right!=null) currEmpty.push(currNode.right);
+                }else{
+                    if(currNode.right!=null) currEmpty.push(currNode.right);  
+                    if(currNode.left!=null) currEmpty.push(currNode.left);
+                }
+
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
+
 class ZigZagLevel{
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result=new LinkedList<>();
