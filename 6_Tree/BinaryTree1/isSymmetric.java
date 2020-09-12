b7a66c46aee969ac814601e7ce651afe0e980139
arunbhati1994 <arunbhati65@gmail.com>
2020-09-12 16:00:41 +0530

    BST,BT,Marics DFS - Flood Fill

--- 6_Tree/BinaryTree1/isSymmetric.java
+++ 6_Tree/BinaryTree1/isSymmetric.java
@@ -1,3 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return helper(root.left,root.right);
+    }    
+    
+    boolean helper(TreeNode left,TreeNode right){
+        if(left==null && right==null) return true;
+        if(left==null || right==null) return false;
+        if(left.val!=right.val) return false;
+        if(!helper(left.left,right.right)) return false;
+        if(!helper(left.right,right.left)) return false;    
+        return true;
+    }
+ 
+}
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return helper(root,root);
