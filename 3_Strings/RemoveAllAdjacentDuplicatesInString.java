317d7c492daa14363c03c54c9f56d9005a22d058
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2020-09-23 11:34:55 +0530

    Update RemoveAllAdjacentDuplicatesInString.java

--- 3_Strings/RemoveAllAdjacentDuplicatesInString.java
+++ 3_Strings/RemoveAllAdjacentDuplicatesInString.java
@@ -8,11 +8,11 @@ Return the final string after all such duplicate removals have been made.  It is
 class Solution {
     public String removeDuplicates(String s) {
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();++i){
-            if(sb.length()!=0 && s.charAt(i)==sb.charAt(sb.length()-1)){
+        for(char character:s.toCharArray()){
+            if(sb.length()!=0 && character==sb.charAt(sb.length()-1)){
                 sb.deleteCharAt(sb.length()-1);
             }else{
-                sb.append(s.charAt(i));
+                sb.append(character);
             }
         }
         return sb.toString();
