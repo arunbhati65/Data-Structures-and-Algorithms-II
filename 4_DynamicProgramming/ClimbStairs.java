36bfccb8f233dc2ff1142747d6fb98e7bd271c31
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2020-10-05 18:23:16 +0530

    Update ClimbStairs.java

--- 4_DynamicProgramming/ClimbStairs.java
+++ 4_DynamicProgramming/ClimbStairs.java
@@ -1,4 +1,19 @@
 class Solution {
+    Integer[] ways;
+    public int climbStairs(int n) {
+        ways=new Integer[n+1];
+        ways[0]=0;
+        if(n<3) return n;
+        ways[1]=1;
+        ways[2]=2;
+        for(int i=3;i<=n;++i){
+            ways[i]=ways[i-1]+ways[i-2];
+        }
+        return ways[n];
+    }
+}
+
+class Solution {
     Integer counter[]=null;
     public int climbStairs(int n) {
         counter=new Integer[n+1];
