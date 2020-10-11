ce68dd6f14c5a2b60f5e45005fecef2bab9012f4
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2020-10-11 10:52:30 +0530

    Update SetMatrixZeroes.java

--- 4_Matrix/SetMatrixZeroes.java
+++ 4_Matrix/SetMatrixZeroes.java
@@ -1,6 +1,53 @@
 //Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
 class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean row=false,col=false;
+        for(int i=0;i<matrix.length;++i){
+            if(matrix[i][0]==0){
+                col=true;
+                break;
+            }
+        }
+        for(int j=0;j<matrix[0].length;++j){
+            if(matrix[0][j]==0){
+                row=true;
+                break;
+            }
+        }
+            
+        for(int i=0;i<matrix.length;++i){
+            for(int j=0;j<matrix[0].length;++j){
+              if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+              }  
+            }
+        }
+        for(int i=1;i<matrix.length;++i){
+            for(int j=1;j<matrix[0].length;++j){
+                  if(matrix[i][0]==0 || matrix[0][j]==0){
+                      matrix[i][j]=0;
+                  }  
+            }
+        }
+        if(col){
+            for(int i=0;i<matrix.length;++i){
+                matrix[i][0]=0;
+            }            
+        }
+
+        if(row){
+            for(int j=0;j<matrix[0].length;++j){
+                matrix[0][j]=0;
+            }            
+        }
+
+    }  
+}
+
+
+class Solution {//Wrong Approach
+    public void setZeroes(int[][] matrix) {
         for(int i=0;i<matrix.length;++i){
             for(int j=0;j<matrix[0].length;++j){
                 if(matrix[i][j]==0){
