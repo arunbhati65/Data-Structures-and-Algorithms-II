01d5a92d4563d73ae8e266136f1ab807c14ce95c
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2020-11-18 22:44:44 +0530

    Update AllPathsFromSorceToDst.java

--- 4_Graph/AllPathsFromSorceToDst.java
+++ 4_Graph/AllPathsFromSorceToDst.java
@@ -2,38 +2,27 @@ class Solution {
     List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         boolean[] visited=new boolean[graph.length];
-        Stack<Integer> s=new Stack<>();
-        s.push(0);
+        LinkedList<Integer> s=new LinkedList<>();
+        s.add(0);
         DFS(graph,0,s,visited);
         return ans;
     }
     
-    void DFS(int[][] graph,int u,Stack<Integer> s,boolean[] visited){
+    void DFS(int[][] graph,int u,LinkedList<Integer> s,boolean[] visited){
         visited[u]=true;
         if(u==graph.length-1){
-            List<Integer> list=new ArrayList<>();
-            printStack(s,list);
+            ans.add(new ArrayList<Integer>(s));
             visited[u]=false;
             return;
         }
         for(int v:graph[u]){
             if(visited[v]==false){
-                s.push(v);
+                s.add(v);
                 DFS(graph,v,s,visited);
-                s.pop();
+                s.remove(s.size()-1);
             }
         }
         visited[u]=false;
     }
     
-    void printStack(Stack<Integer> s,List<Integer> list){
-        if(s.isEmpty()){
-            ans.add(list);
-            return;
-        }
-        int top=s.pop();
-        list.add(0,top);
-        printStack(s,list);
-        s.push(top);
-    }
 }
