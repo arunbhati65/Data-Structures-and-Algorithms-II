8a93f2e2a0a2cf240d0b4f9c05973229a55fe353
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2020-11-20 22:06:42 +0530

    Update firstMissingPositiveInteger.java

--- 2_Maths/firstMissingPositiveInteger.java
+++ 2_Maths/firstMissingPositiveInteger.java
@@ -22,39 +22,3 @@ class Solution {//100
         return nums.length+1;
     }
 }
-
-
-class Solution {//50%
-    public int firstMissingPositive(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;++i){
-            if(!(nums[i]<=0 || nums[i]>nums.length)){
-                set.add(nums[i]);
-            }
-        }
-        for(int i=1;i<=nums.length;++i){
-            if(!set.contains(i)){
-                return i;
-            }
-        }
-        return nums.length+1;
-    }
-}
-
-class Solution { //50%
-    public int firstMissingPositive(int[] nums) {
-        Set<Integer> set=new HashSet();
-        for(int num:nums){
-            set.add(num);
-        }
-        int firstPosNumberMissing=1;
-        for(int num:nums){
-            if(num==firstPosNumberMissing){
-                do{
-                   firstPosNumberMissing++; 
-                }while(set.contains(firstPosNumberMissing));
-            }
-        }
-        return firstPosNumberMissing;
-    }
-}
