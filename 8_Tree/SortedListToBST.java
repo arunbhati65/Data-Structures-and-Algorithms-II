f4fde978562616b558a7c32bca74187b89a26592
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2021-01-01 11:56:41 +0530

    Update SortedListToBST.java

--- 8_Tree/SortedListToBST.java
+++ 8_Tree/SortedListToBST.java
@@ -1,49 +1,29 @@
-class Solution {//100
+class Solution {
     public TreeNode sortedListToBST(ListNode head) {
-        if(head==null) return null;
         return helper(head);
     }
     
-    TreeNode helper(ListNode l){
-        if(l==null) return null;
-        if(l.next==null){
-            return new TreeNode(l.val);
-        }
-        ListNode middle=findMiddle(l);
-        TreeNode newNode=new TreeNode(middle.next.val);
-        ListNode right=middle.next.next;
+    TreeNode helper(ListNode node){
+        if(node==null) return null;
+        if(node.next==null) return new TreeNode(node.val);
+        ListNode prev=findMiddle(node);
+        ListNode middle=prev.next;
+        prev.next=null;
+        ListNode next=middle.next;
         middle.next=null;
-        newNode.left=helper(l);
-        newNode.right=helper(right);
-        return newNode;
+        TreeNode root=new TreeNode(middle.val);
+        root.left=helper(node);
+        root.right=helper(next);
+        return root;
     }
     
     ListNode findMiddle(ListNode node){
-        ListNode ptr1=node,ptr2=node,prev=node;
-        while(ptr2!=null && ptr2.next!=null){
-            prev=ptr1;
-            ptr1=ptr1.next;
-            ptr2=ptr2.next.next;
+        ListNode f=node,s=node,prev=null;
+        while(f!=null && f.next!=null){
+            f=f.next.next;
+            prev=s;
+            s=s.next;
         }
         return prev;
-    }
-}
-
-class Solution {//100
-    public TreeNode sortedListToBST(ListNode head) {
-        return helper(head);
-    }
-    
-    TreeNode helper(ListNode head){
-        if(head==null) return null; 
-        ListNode fast=head,slow=head,tail=null;
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next; tail=slow; slow=slow.next;
-        }
-        if(tail!=null){ tail.next=null; }
-        TreeNode root=new TreeNode(slow.val);
-        if(head==slow) return root;
-        root.left=helper(head); root.right=helper(slow.next);
-        return root;
     }
 }
