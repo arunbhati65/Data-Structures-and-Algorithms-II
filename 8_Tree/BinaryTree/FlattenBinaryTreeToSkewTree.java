11b159baef50ff3b104dfdcd9daec0abb0fe7498
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2021-01-01 10:31:17 +0530

    Update FlattenBinaryTreeToSkewTree.java

--- 8_Tree/BinaryTree/FlattenBinaryTreeToSkewTree.java
+++ 8_Tree/BinaryTree/FlattenBinaryTreeToSkewTree.java
@@ -1,42 +1,15 @@
 class Solution {
-    TreeNode curr;
-    public TreeNode increasingBST(TreeNode root) {
-        curr=new TreeNode();
-        TreeNode dummy=curr;
-        helper(root);
-        return dummy.right;
-    }
-    
-    void helper(TreeNode node){
-        if(node==null) return ;
-        helper(node.left);
-        node.left=null;
-        curr.right=node;
-        curr=node;
-        helper(node.right);        
-    }
-}
-
-class Solution {//Inorder traversal
     public void flatten(TreeNode root) {
-        if(root==null) return;
-        helper(root);
-    }
-    
-    TreeNode helper(TreeNode node){
-        if(node==null) return node;
-        TreeNode root=node;
-        TreeNode left=helper(node.left);
-        node.left=null;
-        TreeNode next=node.right;
-        if(left!=null){
-            node.right=left; 
-            while(node.right!=null){
-                node=node.right;
-            }
-            node.right=next;
+        if(root==null) return ;
+        TreeNode right=root.right;
+        flatten(root.left);
+        root.right=root.left;
+        root.left=null;
+        flatten(right);
+        TreeNode temp=root;
+        while(temp.right!=null){
+            temp=temp.right;
         }
-        helper(next);
-        return root;
+        temp.right=right;
     }
 }
