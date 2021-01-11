40817da8fe1f217da749c783a67416599cfe7c1c
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2021-01-11 12:40:28 +0530

    Find first missing number

--- 8_Array/FindFirstMissingNumber.java
+++ 8_Array/FindFirstMissingNumber.java
@@ -14,3 +14,23 @@ class Solution {
         return number;
     }
 }
+
+class Solution {
+    public int solution(int[] A) {
+        for(int i=0;i<A.length;++i){
+            if(A[i]<0){
+                A[i]=0;
+            }
+        }
+        for(int i=0;i<A.length;++i){
+            if(A[i]==0 || A[Math.abs(A[i])-1]<0) continue;
+            A[Math.abs(A[i])-1]=-1*A[Math.abs(A[i])-1];
+        }
+       
+        int i=0;
+        for(i=0;i<A.length;++i){
+            if(A[i]>=0) return i+1;
+        }
+        return i+1;
+    }
+}
