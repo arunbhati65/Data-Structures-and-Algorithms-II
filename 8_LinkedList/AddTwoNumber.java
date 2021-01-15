54c662196c4e076c5f0cbf645ca85ca11c9602e7
IAF <38494947+StackUnderfloww@users.noreply.github.com>
2021-01-15 16:48:23 +0530

    Update AddTwoNumber.java

--- 8_LinkedList/AddTwoNumber.java
+++ 8_LinkedList/AddTwoNumber.java
@@ -10,6 +10,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Queue<Integer> q1=new LinkedList<>(),q2=new LinkedList<>();
+        while(l1!=null){
+            q1.add(l1.val);
+            l1=l1.next;
+        }
+        while(l2!=null){
+            q2.add(l2.val);
+            l2=l2.next;
+        }
+        ListNode dummy=new ListNode();
+        ListNode temp=dummy;
+        int carry=0;
+        while(!q1.isEmpty() || !q2.isEmpty() || carry!=0){
+            int x=q1.isEmpty()?0:q1.poll();
+            int y=q2.isEmpty()?0:q2.poll();
+            int sum=(x+y+carry)%10;
+            carry=(x+y+carry)/10;
+            temp.next=new ListNode(sum);
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy=new ListNode();
         ListNode curr=dummy;
         int carry=0;
