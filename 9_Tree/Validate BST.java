3fc38eb340da8a64bd9dcf398954568bf3fe6951
Arun Bhati <38494947+arunbhati65@users.noreply.github.com>
2025-01-24 18:17:08 +0530

    Update Validate BST.java
    without using static variable

--- 9_Tree/Validate BST.java
+++ 9_Tree/Validate BST.java
@@ -1,3 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        TreeNode[] prev=new TreeNode[1];
+        return helper(root,prev); 
+    }
+
+    boolean helper(TreeNode node,TreeNode[] prev){
+        if(node==null) return true;
+        if(!helper(node.left,prev)) return false;
+        if(prev[0]!=null && node.val<=prev[0].val) return false;
+        prev[0]=node;
+        if(!helper(node.right,prev)) return false;
+        return true;
+    }
+}
+
+
 class Solution {
     Integer prev=null;
     public boolean isValidBST(TreeNode root) {
